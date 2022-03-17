d5536d93f64654c1edce9ca316c470f49588ecf8
MB-teacher <MB-teacher@MB-207-teacher>
2022-03-17 19:14:14 +0800

    ocp

--- src/main/java/day02/Student.java
+++ src/main/java/day02/Student.java
@@ -1,24 +1,36 @@
 package day02;
-
+// 設計一個學生類
 public class Student {
-    String name; // 姓名
-    double height; // 身高
-    double weight; // 體重
-    int score; // 分數
+    private String name; // 姓名
+    private double height; // 身高
+    private double weight; // 體重
+    private int score; // 分數
     // 設定姓名
     void setName(String name) {
         this.name = name;
     }
     // 設定身高
     void setHeight(double h) {
+        if(h <= 0 || h>250) {
+            System.out.println("身高資料錯誤");
+            return;
+        }
         height = h;
     }
     // 設定體重
     void setWeight(double w) {
+        if(w <= 0 || w>300) {
+            System.out.println("體重資料錯誤");
+            return;
+        }
         weight = w;
     }
     // 設定分數
     void setScore(int score) {
+        if(score < 0 || score>100) {
+            System.out.println("分數資料錯誤");
+            return;
+        }
         this.score = score;
     }
     // 印出學生資料
