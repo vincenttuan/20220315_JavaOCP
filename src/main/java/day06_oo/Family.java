866e17009f103a7b4eeebe6867bdef9fb1257147
MB-teacher <MB-teacher@MB-207-teacher>
2022-03-31 21:38:13 +0800

    ocp

---
+++ src/main/java/day06_oo/Family.java
@@ -0,0 +1,28 @@
+package day06_oo;
+
+class Father {
+    Father() {
+        System.out.println("F1");
+    }
+    Father(int age) {
+        this();
+        System.out.println("F2, age:" + age);
+    }
+}
+
+class Son extends Father {
+    Son() {
+        this(18);
+        System.out.println("S1");
+    }
+    Son(int age) {
+        super(age);
+        System.out.println("S2, age:" + age);
+    }
+}
+
+public class Family {
+    public static void main(String[] args) {
+        Son son = new Son();
+    }
+}
