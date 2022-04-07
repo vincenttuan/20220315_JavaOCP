82f7f7b1c5be74b3f2e2652f98b18e58094897bc
MB-teacher <MB-teacher@MB-207-teacher>
2022-04-07 19:11:47 +0800

    ocp

--- src/main/java/day07_oo/OverrideDemo.java
+++ src/main/java/day07_oo/OverrideDemo.java
@@ -1,12 +1,13 @@
 package day07_oo;
 
 class Father {
+    int x = 100;
     void play() {
         System.out.println("Golf");
     }
 }
-
 class Son extends Father {
+    int x = 200; // 遮蔽 Father 的屬性(物件變數)
     @Override
     void play() { // 覆寫 Father 的方法
         System.out.println("LoL");
@@ -15,12 +16,11 @@ class Son extends Father {
         System.out.println("Hello son");
     }
 }
-
-
 public class OverrideDemo {
     public static void main(String[] args) {
         Son son = new Son();
         son.play();
         son.hello();
+        System.out.println(son.x);
     }
 }
