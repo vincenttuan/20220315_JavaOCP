81a018b09f9aaef5cb1e24f36d12125bff68eb33
MB-teacher <MB-teacher@MB-207-teacher>
2022-04-12 19:56:44 +0800

    ocp

---
+++ src/main/java/day08_oo/Teacher.java
@@ -0,0 +1,14 @@
+package day08_oo;
+
+public class Teacher {
+    private int salary;
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+    
+}
