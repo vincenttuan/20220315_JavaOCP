656109fd9dea5a4a84212407a355ba91434d5d4c
MB-teacher <MB-teacher@MB-207-teacher>
2022-04-12 20:00:04 +0800

    ocp

--- src/main/java/day08_oo/Student.java
+++ src/main/java/day08_oo/Student.java
@@ -2,7 +2,16 @@ package day08_oo;
 
 public class Student extends Person {
     private int score;
-
+    
+    public Student() {
+    }
+    
+    public Student(String name, int age, int score) {
+        setName(name);
+        setAge(age);
+        setScore(score);
+    }
+    
     public int getScore() {
         return score;
     }
--- src/main/java/day08_oo/Teacher.java
+++ src/main/java/day08_oo/Teacher.java
@@ -3,6 +3,15 @@ package day08_oo;
 public class Teacher extends Person {
     private int salary;
 
+    public Teacher() {
+    }
+    
+    public Teacher(String name, int age, int salary) {
+        setName(name);
+        setAge(age);
+        setSalary(salary);
+    }
+    
     public int getSalary() {
         return salary;
     }
