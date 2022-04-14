156ba6c63867140f719b09444d3b1ee0e1bd00d8
MB-teacher <MB-teacher@MB-207-teacher>
2022-04-14 19:14:18 +0800

    ocp

--- src/main/java/access/foo/Greet2.java
+++ src/main/java/access/foo/Greet2.java
@@ -2,12 +2,12 @@ package access.foo;
 
 import access.bar.Hello;
 
-public class Greet {
+public class Greet2 extends Hello {
     // 存取 Hello 的資源
     public void accessHello() {
-        Hello hello = new Hello();
+        Greet2 hello = new Greet2();
         System.out.println(hello.a);
-        //System.out.println(hello.b); // 不可呼叫 Hello protected 方法
+        System.out.println(hello.b); // 可呼叫 Hello protected 方法
         //System.out.println(hello.c); // 不可呼叫 Hello 預設 方法
         //System.out.println(hello.d); // 不可呼叫 Hello private 方法
     }
