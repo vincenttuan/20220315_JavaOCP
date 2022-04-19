edb7f3051235c94a60737b92362b76c0a94358dd
MB-teacher <MB-teacher@MB-207-teacher>
2022-04-19 19:34:01 +0800

    ocp

---
+++ src/main/java/day10_oo3/EDog.java
@@ -0,0 +1,15 @@
+package day10_oo3;
+
+public class EDog implements Dog {
+
+    @Override
+    public void skill() {
+        System.out.println("玩遊戲");
+    }
+
+    @Override
+    public void eat() {
+        System.out.println("吃電");
+    }
+    
+}
--- src/main/java/day10_oo3/PetStore.java
+++ src/main/java/day10_oo3/PetStore.java
@@ -1,9 +1,12 @@
 package day10_oo3;
 
+import java.util.stream.Stream;
+
 public class PetStore {
     public static void main(String[] args) {
         Dog dog1 = new Shiba();
         Dog dog2 = new Chihuahua();
+        Dog dog3 = new EDog();
         System.out.println(Dog.LEGS);
         System.out.println(dog1.getClass().getSimpleName());
         System.out.println(dog2.getClass().getSimpleName());
@@ -11,6 +14,14 @@ public class PetStore {
         dog1.eat();
         dog2.skill();
         dog2.eat();
+        dog3.skill();
+        dog3.eat();
+        // Java 8
+        Dog[] dogs = {dog1, dog2, dog3};
+        Stream.of(dogs).forEach(dog -> {
+            dog.skill();
+            dog.eat();
+        });
         
     }
 }
