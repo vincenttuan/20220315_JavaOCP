33a7bab960e05ff8316c89cb88fa1477d81e211d
MB-teacher <MB-teacher@MB-207-teacher>
2022-04-19 18:58:53 +0800

    ocp

---
+++ src/main/java/day10_oo3/Chihuahua.java
@@ -0,0 +1,15 @@
+package day10_oo3;
+
+public class Chihuahua implements Dog {
+
+    @Override
+    public void skill() {
+        System.out.println("療欲");
+    }
+
+    @Override
+    public void eat() {
+         System.out.println("寶路狗食");
+    }
+    
+}
