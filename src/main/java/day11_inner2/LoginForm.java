ce1be2963e49428264a3dcece169c2777c6d305d
MB-teacher <MB-teacher@MB-207-teacher>
2022-04-21 19:49:06 +0800

    ocp

--- src/main/java/day11_inner2/LoginForm.java
+++ src/main/java/day11_inner2/LoginForm.java
@@ -8,11 +8,14 @@ public class LoginForm {
     }
     // 自動登入
     public void auto() {
-        String title = "自動登入";
+        String title = "自動登入"; // 區域變數
         // 此 Button 是 auto() 方法所獨享的
+        // 方法內部類別
         class Button { // 不可以加入權限的修飾字
             void action() {
                 System.out.println(userName);
+                // 因方法內部類別有使用 title 區域變數
+                // 則 title 區域變數在編譯時會自動加上 final 修飾
                 System.out.println(title);
             }
         }
