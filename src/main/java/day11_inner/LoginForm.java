2e3982127bcb94a2c00fa0a3d3a318e80e0191f9
MB-teacher <MB-teacher@MB-207-teacher>
2022-04-21 19:30:12 +0800

    ocp

--- src/main/java/day11_inner/LoginForm.java
+++ src/main/java/day11_inner/LoginForm.java
@@ -3,16 +3,22 @@ package day11_inner;
 // 外部類別(表單)
 public class LoginForm {
     private String userName;
-
+    private String title = "登入表單"; // 表單抬頭
     public LoginForm(String userName) {
         this.userName = userName;
     }
     
     // 內部類別(按鈕)
     public class Button {
+        private String title = "傳送"; // 按鈕抬頭
         // 行為
         public void action() {
+            String title = "驗證成功"; // 行為抬頭
+            System.out.println(LoginForm.this.title); // 得到 -> 登入表單
+            System.out.println(this.title); // 得到 -> 傳送
+            System.out.println(title); // 得到 -> 驗證成功
             System.out.println(userName + "登入了...");
+            
         }
     }
     
