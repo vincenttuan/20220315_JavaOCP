5b052426af4a0ee5bce79c639f9ab15625e1e6c2
MB-teacher <MB-teacher@MB-207-teacher>
2022-04-28 20:51:31 +0800

    ocp

--- src/main/java/day13_set/Exam.java
+++ src/main/java/day13_set/Exam.java
@@ -1,5 +1,7 @@
 package day13_set;
 
+import java.util.Objects;
+
 public class Exam {
     private String subject; // 科目
     private Integer score; // 分數
@@ -25,6 +27,37 @@ public class Exam {
         this.score = score;
     }
 
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 79 * hash + Objects.hashCode(this.subject);
+        hash = 79 * hash + Objects.hashCode(this.score);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Exam other = (Exam) obj;
+        if (!Objects.equals(this.subject, other.subject)) {
+            return false;
+        }
+        if (!Objects.equals(this.score, other.score)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+    
     @Override
     public String toString() {
         return "Exam{" + "subject=" + subject + ", score=" + score + '}';
--- src/main/java/day13_set/SetDemoExam.java
+++ src/main/java/day13_set/SetDemoExam.java
@@ -8,12 +8,14 @@ public class SetDemoExam {
         Exam e1 = new Exam("國文", 100);
         Exam e2 = new Exam("英文", 70);
         Exam e3 = new Exam("數學", 90);
+        Exam e4 = new Exam("數學", 90);
         
         Set<Exam> exams = new LinkedHashSet<>();
         exams.add(e1);
         exams.add(e2);
         exams.add(e3);
-        
+        exams.add(e4);
+        System.out.println(exams.size());
         System.out.println(exams);
         
         
