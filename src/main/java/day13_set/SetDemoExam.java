5c74aa95949e83fe2ac33eff074b54cbb2e980f8
MB-teacher <MB-teacher@MB-207-teacher>
2022-04-28 21:11:42 +0800

    ocp

--- src/main/java/day13_set/SetDemoExam.java
+++ src/main/java/day13_set/SetDemoExam.java
@@ -2,6 +2,8 @@ package day13_set;
 
 import java.util.LinkedHashSet;
 import java.util.Set;
+import static java.util.stream.Collectors.toSet;
+
 
 public class SetDemoExam {
     public static void main(String[] args) {
@@ -26,7 +28,20 @@ public class SetDemoExam {
         int max = exams.stream().mapToInt(Exam::getScore).max().getAsInt();
         System.out.println(max);
         
-        // 請求出最高分的科目 = ?
+        // 請求出最高分的科目 = ? Java 7
+        Set<String> subjects = new LinkedHashSet<>(); // 儲存最高分的科目
+        for(Exam exam : exams) {
+            if(exam.getScore() == max) {
+                subjects.add(exam.getSubject());
+            }
+        }
+        System.out.println(subjects);
+        
+        // 請求出最高分的科目 = ? Java 8
+        Set<String> subjects2 =  exams.stream()
+                                      .filter(exam -> exam.getScore() == max)
+                                      .map(exam -> exam.getSubject()) // ["國文", "數學"]
+                                      .collect(toSet()); // 轉 Set 集合
         
     }
 }
