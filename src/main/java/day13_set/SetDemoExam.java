9bb7bfcf482e7645e3770500f1823818d46d2d6b
MB-teacher <MB-teacher@MB-207-teacher>
2022-04-28 21:02:05 +0800

    ocp

--- src/main/java/day13_set/SetDemoExam.java
+++ src/main/java/day13_set/SetDemoExam.java
@@ -7,17 +7,26 @@ public class SetDemoExam {
     public static void main(String[] args) {
         Exam e1 = new Exam("國文", 100);
         Exam e2 = new Exam("英文", 70);
-        Exam e3 = new Exam("數學", 90);
-        Exam e4 = new Exam("數學", 90);
+        Exam e3 = new Exam("數學", 100);
         
         Set<Exam> exams = new LinkedHashSet<>();
         exams.add(e1);
         exams.add(e2);
         exams.add(e3);
-        exams.add(e4);
         System.out.println(exams.size());
         System.out.println(exams);
         
+        // 請求出總分 = ?
+        int sum1 = exams.stream().mapToInt(exam -> exam.getScore()).sum();
+        int sum2 = exams.stream().mapToInt(Exam::getScore).sum();
+        System.out.println(sum1);
+        System.out.println(sum2);
+                
+        // 請求出最高分 = ?
+        int max = exams.stream().mapToInt(Exam::getScore).max().getAsInt();
+        System.out.println(max);
+        
+        // 請求出最高分的科目 = ?
         
     }
 }
