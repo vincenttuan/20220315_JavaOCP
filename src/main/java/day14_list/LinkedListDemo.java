d741e53d1bd2dde813ba3cd2f117cbbbf882e4fb
MB-teacher <MB-teacher@MB-207-teacher>
2022-05-03 19:22:07 +0800

    ocp

---
+++ src/main/java/day14_list/LinkedListDemo.java
@@ -0,0 +1,33 @@
+package day14_list;
+
+// LinkedList 有較快的新增(插入)/修改/刪除的效能
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ListIterator;
+
+// LinkedList 查詢速度慢於 ArrayList
+public class LinkedListDemo {
+    public static void main(String[] args) {
+        List<Integer> scores = new LinkedList<>();
+        scores.add(100);
+        scores.add(90);
+        scores.add(90);
+        scores.add(0, 80); // 將 80 放入到第一個位置
+        scores.add(60);
+        System.out.println(scores);
+        // 透過 ListIterator 來輪詢 scores
+        ListIterator<Integer> iter = scores.listIterator();
+        while (iter.hasNext()) {
+            int index = iter.nextIndex();
+            Integer next = iter.next();
+            System.out.printf("%d : %d\n", index, next);
+        }
+        System.out.println("-------------");
+        while (iter.hasPrevious()) {
+            int index = iter.previousIndex();
+            Integer previous = iter.previous();
+            System.out.printf("%d : %d\n", index, previous);
+        }
+    }
+}
