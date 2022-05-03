a0ab7c9ddc2a082e14cb6086834511794a1f8ed8
MB-teacher <MB-teacher@MB-207-teacher>
2022-05-03 20:03:09 +0800

    ocp

--- src/main/java/day14_list/LinkedListDemo4.java
+++ src/main/java/day14_list/LinkedListDemo4.java
@@ -10,16 +10,25 @@ public class LinkedListDemo4 {
     public static void main(String[] args) {
         Exam e1 = new Exam("Java", 100);
         Exam e2 = new Exam("Java", 100);
+        Exam e7 = new Exam("Python", 70);
+        Exam e8 = new Exam("Python", 93);
         Exam e3 = new Exam("VB", 90);
         Exam e4 = new Exam("Java", 80);
         Exam e5 = new Exam("VB", 95);
         Exam e6 = new Exam("VB", 90);
-        List<Exam> exams = Arrays.asList(e1, e2, e3, e4, e5, e6);
+        List<Exam> exams = Arrays.asList(e1, e2, e3, e4, e5, e6, e7, e8);
         // 試問 Java 與 VB 的平均分數各是多少?
+        /*
         Set<String> subjectNames = exams.stream()
                 .map(e -> e.getSubject())
                 .collect(Collectors.toSet());
+        
         subjectNames.forEach(subjectName -> System.out.printf("%s %.1f\n", subjectName, getAvg(subjectName, exams)));
+        */
+        exams.stream()
+                .map(e -> e.getSubject())
+                .collect(Collectors.toSet())
+                .forEach(subjectName -> printScore(subjectName, getAvg(subjectName, exams)));
     }
     
     public static double getAvg(String subjectName, List<Exam> exams) {
@@ -28,6 +37,10 @@ public class LinkedListDemo4 {
                     .mapToInt(Exam::getScore) // e -> e.getScore()
                     .average()
                     .getAsDouble();
+    }
+    
+    public static void printScore(String subjectName, double avg) {
+        System.out.printf("%s %.1f\n", subjectName, avg);
     }
     
 }
