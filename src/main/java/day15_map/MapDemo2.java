33a01231672a4dc0a7895d93acc54059c538394e
MB-teacher <MB-teacher@MB-207-teacher>
2022-05-05 19:28:10 +0800

    ocp

---
+++ src/main/java/day15_map/MapDemo2.java
@@ -0,0 +1,11 @@
+package day15_map;
+
+public class MapDemo2 {
+    public static void main(String[] args) {
+        String[] fruits = {"apple", "orange", "watermelon", "watermelon", "apple", "apple", "apple", "banana"};
+        // 試問每一種水果有幾個?
+        // 結果範例: {apple=4, watermelon=2, orange=1, banana=1}
+        
+        
+    }
+}
