1d8d12087c66404ceef3649d3662035b87b50c0e
MB-teacher <MB-teacher@MB-207-teacher>
2022-05-05 20:43:14 +0800

    ocp

--- src/main/java/day15_map/MapDemo3.java
+++ src/main/java/day15_map/MapDemo3.java
@@ -8,18 +8,19 @@ import java.util.stream.Collectors;
 import static java.util.function.Function.identity;
 import static java.util.stream.Collectors.counting;
 import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.summingInt;
 
 public class MapDemo3 {
     public static void main(String[] args) {
         String[][] fruits = {
             {"apple",  "10", "1.99"}, // 此箱有 10 個 apple 每個 1.99 元
             {"orange", "20", "2.99"},
-            {"melon",  "15", "3.99"}, 
+            {"papaya", "15", "3.99"}, 
             {"melon",  "30", "3.99"},  
             {"apple",  "15", "1.99"},
             {"apple",  "20", "1.99"}, 
             {"apple",  "25", "1.99"},
-            {"banana", "50", "0.99"} 
+            {"banana", "50", "1.99"} 
         };
         // 試問每一種水果各有幾箱?
         // 結果範例: {apple=4, watermelon=2, orange=1, banana=1}
@@ -28,12 +29,20 @@ public class MapDemo3 {
                 .collect(Collectors.toList());
         System.out.println(list);
         Map<String, Long> resultMap1 = list.stream()
-                .collect(groupingBy(fruit -> fruit.getName(), counting()));
+                .collect(groupingBy(Fruit::getName, counting()));
         System.out.println(resultMap1);
         //----------------------------------------------------------------
         Map<String, Long> resultMap2 = Arrays.stream(fruits)
                 .map(array -> new Fruit(array))
-                .collect(groupingBy(fruit -> fruit.getName(), counting()));
+                .collect(groupingBy(Fruit::getName, counting()));
         System.out.println(resultMap2);
+        //----------------------------------------------------------------
+        // 試問每一種水果各有多少個?
+        // 每箱數量加總
+        Map<String, Integer> resultMap3 = Arrays.stream(fruits)
+                .map(array -> new Fruit(array))
+                .collect(groupingBy(Fruit::getName, summingInt(Fruit::getCount)));
+        System.out.println(resultMap3);
+        
     }
 }
