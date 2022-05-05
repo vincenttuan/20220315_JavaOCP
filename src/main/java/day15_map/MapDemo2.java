6af3f05eb5f5c93946423b426a42fe6e7bac4a6f
MB-teacher <MB-teacher@MB-207-teacher>
2022-05-05 19:37:41 +0800

    ocp

--- src/main/java/day15_map/MapDemo2.java
+++ src/main/java/day15_map/MapDemo2.java
@@ -1,10 +1,22 @@
 package day15_map;
 
+import java.util.Arrays;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import static java.util.function.Function.identity;
+import static java.util.stream.Collectors.counting;
+import static java.util.stream.Collectors.groupingBy;
+
 public class MapDemo2 {
     public static void main(String[] args) {
         String[] fruits = {"apple", "orange", "watermelon", "watermelon", "apple", "apple", "apple", "banana"};
         // 試問每一種水果有幾個?
         // 結果範例: {apple=4, watermelon=2, orange=1, banana=1}
+        Map<String, Long> resultMap = Arrays.stream(fruits)
+                //.collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+                .collect(groupingBy(identity(), counting()));
+        System.out.println(resultMap);
         
         
     }
