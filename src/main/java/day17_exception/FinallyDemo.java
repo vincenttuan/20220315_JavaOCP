8f43d1e1f12935a7d6521dc1d74239e5086bb3eb
MB-teacher <MB-teacher@MB-207-teacher>
2022-05-12 20:01:05 +0800

    ocp

---
+++ src/main/java/day17_exception/FinallyDemo.java
@@ -0,0 +1,22 @@
+package day17_exception;
+
+public class FinallyDemo {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 2;
+        int result = 0;
+        try {
+            result = x / y;
+            System.out.println("result=" + result); 
+            //return;  // 方法中止 (仍會執行 finally 區段程式碼)
+            System.exit(0); // 強迫離開 (不會執行 finally 區段程式碼)
+        } catch (ArithmeticException e) {
+            System.out.println(e);
+            System.out.println(e.getMessage());
+            return;  // 方法中止 (仍會執行 finally 區段程式碼)
+        } finally {
+            System.out.println("程式結束"); // 一定要執行
+        }
+        
+    }
+}
