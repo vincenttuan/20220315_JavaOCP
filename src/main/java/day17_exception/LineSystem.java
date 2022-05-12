30dcf97c32aa07ecbe4f1cbf5cadb062d3402ad6
MB-teacher <MB-teacher@MB-207-teacher>
2022-05-12 19:37:36 +0800

    ocp

--- src/main/java/day17_exception/LineSystem.java
+++ src/main/java/day17_exception/LineSystem.java
@@ -5,7 +5,9 @@ public class LineSystem {
         LineService lineService = new LineService();
         int statusCode = 0;
         try {
-            statusCode = lineService.sendSticker("貼圖", 11537, 52002753);
+            //statusCode = lineService.sendSticker("貼圖", 11537, 52002753);
+            String imageUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bf/F-16C_Fighting_Falcon.JPEG/300px-F-16C_Fighting_Falcon.JPEG";
+            statusCode = lineService.sendWebImage("Web", imageUrl);
         } catch (Exception ex) {
             System.out.println(ex);
         }
