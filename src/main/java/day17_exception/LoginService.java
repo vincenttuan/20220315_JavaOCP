b867a3e6394128d34acf511a74d53883970d0626
MB-teacher <MB-teacher@MB-207-teacher>
2022-05-12 21:35:48 +0800

    ocp

--- src/main/java/day17_exception/LoginService.java
+++ src/main/java/day17_exception/LoginService.java
@@ -2,4 +2,13 @@ package day17_exception;
 
 public class LoginService {
     
+    public boolean login(String username, String password) throws LoginException {
+        if(username.equals("admin") && password.equals("1234")) {
+            return true;
+        } else {
+            LoginException loginException = new LoginException("登入失敗");
+            throw loginException;
+        }
+    }
+    
 }
