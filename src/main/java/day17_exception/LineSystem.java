10db94d1758fee0861eebf6322de87aa5efa1b49
MB-teacher <MB-teacher@MB-207-teacher>
2022-05-12 19:48:54 +0800

    ocp

--- src/main/java/day17_exception/LineSystem.java
+++ src/main/java/day17_exception/LineSystem.java
@@ -5,7 +5,13 @@ public class LineSystem {
         LineService lineService = new LineService();
         int statusCode = 0;
         try {
-            //statusCode = lineService.sendSticker("貼圖", 11537, 52002753);
+            // 發送文字訊息
+            statusCode = lineService.sendText("Hello...");
+            // 發送Youtube連結
+            statusCode = lineService.sendText("https://www.youtube.com/watch?v=U9VpeXil8u8");
+            // 發送貼圖
+            statusCode = lineService.sendSticker("貼圖", 11537, 52002753);
+            // 發送網路圖片
             String imageUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bf/F-16C_Fighting_Falcon.JPEG/300px-F-16C_Fighting_Falcon.JPEG";
             statusCode = lineService.sendWebImage("Web", imageUrl);
         } catch (Exception ex) {
