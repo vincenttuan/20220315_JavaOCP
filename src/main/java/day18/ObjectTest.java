7819aa99668441a37d701d8fcf4df186508ef52f
MB-teacher <MB-teacher@MB-207-teacher>
2022-05-17 21:26:11 +0800

    ocp

--- src/main/java/day18/ObjectTest.java
+++ src/main/java/day18/ObjectTest.java
@@ -3,14 +3,23 @@ package day18;
 class BigData {
     private String data;
     BigData() {
+        System.out.printf("建構子執行者: %s\n", Thread.currentThread().getName());
         for(int i=0;i<=9999;i++) {
             data += String.valueOf(i);
         }
     }
+
+    @Override
+    protected void finalize() throws Throwable {
+        super.finalize(); //To change body of generated methods, choose Tools | Templates.
+        System.out.printf("finalize 方法執行者: %s\n", Thread.currentThread().getName());
+    }
+    
 }
 
 public class ObjectTest {
     public static void main(String[] args) throws Exception {
+        System.out.printf("main 方法執行者: %s\n", Thread.currentThread().getName());
         Runtime runtime = Runtime.getRuntime();
         System.out.printf("FreeMemory: %,d bytes\n", runtime.freeMemory());
         BigData bigData = new BigData();
