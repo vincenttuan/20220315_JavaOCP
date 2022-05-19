2377daa4ef839234984ede85d00f8f7f48536868
MB-teacher <MB-teacher@MB-207-teacher>
2022-05-19 19:51:05 +0800

    ocp

--- src/main/java/day19_thread/Mintendo.java
+++ src/main/java/day19_thread/Mintendo.java
@@ -3,6 +3,9 @@ package day19_thread;
 public class Mintendo {
     public static void main(String[] args) {
         SuperMario superMario = new SuperMario();
+        GameMusic gameMusic = new GameMusic();
+        gameMusic.setDaemon(true);
         superMario.start();
+        gameMusic.start();
     }
 }
