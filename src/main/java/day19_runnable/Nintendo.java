36f2656707c65accbe3b1288041e212d6a765937
MB-teacher <MB-teacher@MB-207-teacher>
2022-05-19 20:50:48 +0800

    ocp

--- src/main/java/day19_runnable/Nintendo.java
+++ src/main/java/day19_runnable/Nintendo.java
@@ -1,13 +1,19 @@
 package day19_runnable;
 
+import day19_thread.SuperMario;
+
 public class Nintendo {
     public static void main(String[] args) {
         // 準備卡帶
         Mario mario = new Mario();
         Bubble bubble = new Bubble();
         KingKong kingKong = new KingKong();
-        
-        Thread thread = new Thread();
-        thread.start();
+        SuperMario superMario = new SuperMario();
+        Runnable pal = () -> System.out.println("仙劍奇俠傳");;
+        // 主機
+        Thread thread1 = new Thread(kingKong);
+        Thread thread2 = new Thread(pal);
+        thread1.start();
+        thread2.start();
     }
 }
