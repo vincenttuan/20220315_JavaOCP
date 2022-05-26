f94d56baf30890c609be3ab85c1365ee805a7bad
MB-teacher <MB-teacher@MB-207-teacher>
2022-05-26 19:18:19 +0800

    ocp

--- src/main/java/day21/RoomMain.java
+++ src/main/java/day21/RoomMain.java
@@ -8,10 +8,14 @@ public class RoomMain {
         Person p1 = new Person(room);
         Person p2 = new Person(room);
         Person p3 = new Person(room);
+        Person p4 = new Person(room);
+        Person p5 = new Person(room);
+        Person p6 = new Person(room);
         // 設定執行緒名稱
         p1.setName("小明");
         p2.setName("小華");
         p3.setName("小英");
+        
         // 爭先恐後去看診
         p1.start();
         p2.start();
