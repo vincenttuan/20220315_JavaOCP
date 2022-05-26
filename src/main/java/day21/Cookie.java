ba0a4626bfaa6d2e0626e7cb2fbe46a3649455a4
MB-teacher <MB-teacher@MB-207-teacher>
2022-05-26 21:40:22 +0800

    ocp

--- src/main/java/day21/Cookie.java
+++ src/main/java/day21/Cookie.java
@@ -5,7 +5,7 @@ public class Cookie {
     private boolean empty = true;
     
     public synchronized void put(int i) {
-        if(empty == false) {
+        while(empty == false) {
             try {
                 wait();
             } catch (InterruptedException e) {
@@ -14,11 +14,11 @@ public class Cookie {
         }
         System.out.printf("主人放第 %d 塊餅乾\n", i);
         empty = false; // 放好餅乾, 盤子也不為空
-        notify(); // 通知小狗
+        notifyAll(); // 通知小狗
     }
     
     public synchronized void eat(int i) {
-        if(empty == true) {
+        while(empty == true) {
             try {
                 wait();
             } catch (InterruptedException e) {
@@ -27,7 +27,7 @@ public class Cookie {
         }
         System.out.printf("小狗吃第 %d 塊餅乾\n", i);
         empty = true; // 吃完了, 盤子也空了
-        notify(); // 通知主人
+        notifyAll(); // 通知主人
     }
     
 }
