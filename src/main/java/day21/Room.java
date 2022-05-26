55625d8634969f80e405edcf4df4efba63ba0116
MB-teacher <MB-teacher@MB-207-teacher>
2022-05-26 19:15:14 +0800

    ocp

--- src/main/java/day21/Room.java
+++ src/main/java/day21/Room.java
@@ -3,6 +3,14 @@ import java.util.Random;
 
 // 診間
 public class Room {
+    // 填寫資料
+    public void write() throws InterruptedException {
+        Random random = new Random();
+        String name = Thread.currentThread().getName();
+        System.out.printf("%s 開始填寫基本資料\n", name);
+        Thread.sleep(random.nextInt(1000));
+        System.out.printf("%s 完成基本資料填寫\n", name);
+    }
     
     // 看診
     public synchronized void watch() throws InterruptedException {
