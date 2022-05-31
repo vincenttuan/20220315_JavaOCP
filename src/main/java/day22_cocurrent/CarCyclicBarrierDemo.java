958274e5cafc7818ff0d57dd3baddae11361fc6d
MB-teacher <MB-teacher@MB-207-teacher>
2022-05-31 21:36:36 +0800

    ocp

---
+++ src/main/java/day22_cocurrent/CarCyclicBarrierDemo.java
@@ -0,0 +1,15 @@
+package day22_cocurrent;
+
+import java.util.concurrent.CyclicBarrier;
+
+public class CarCyclicBarrierDemo {
+    public static void main(String[] args) {
+        int parties = 3;
+        Runnable lunch = () -> System.out.println(Thread.currentThread().getName() + " 吃午飯");
+        //CyclicBarrier cb = new CyclicBarrier(parties);
+        CyclicBarrier cb = new CyclicBarrier(parties, lunch);
+        new Car(cb).start();
+        new Car(cb).start();
+        new Car(cb).start();
+    }
+}
