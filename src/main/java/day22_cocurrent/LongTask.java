ad79129f3a13887e429beb2aa4c82496ea4c61a8
MB-teacher <MB-teacher@MB-207-teacher>
2022-05-31 19:42:14 +0800

    ocp

---
+++ src/main/java/day22_cocurrent/LongTask.java
@@ -0,0 +1,18 @@
+package day22_cocurrent;
+
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+public class LongTask implements Runnable {
+
+    @Override
+    public void run() {
+        System.out.println("長任務開始");
+        try {
+            TimeUnit.SECONDS.sleep(new Random().nextInt(5) + 2);
+        } catch (Exception e) {
+        }
+        System.out.println("長任務結束");
+    }
+    
+}
