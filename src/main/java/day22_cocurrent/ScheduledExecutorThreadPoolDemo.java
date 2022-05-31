54f1e90f7d13eeb3af7de6f9d25fc01a959e9149
MB-teacher <MB-teacher@MB-207-teacher>
2022-05-31 20:48:32 +0800

    ocp

--- src/main/java/day22_cocurrent/ScheduledExecutorThreadPoolDemo.java
+++ src/main/java/day22_cocurrent/ScheduledExecutorThreadPoolDemo.java
@@ -1,8 +1,16 @@
 package day22_cocurrent;
 
 // 排程化執行緒池
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 public class ScheduledExecutorThreadPoolDemo {
     public static void main(String[] args) {
-        
+        ScheduledExecutorService service = Executors.newSingleThreadScheduledExecutor();
+        // 停留 2 秒鐘之後才執行 Lucky 的工作
+        service.schedule(new Lucky(), 2, TimeUnit.SECONDS);
+        service.shutdown();
     }
 }
