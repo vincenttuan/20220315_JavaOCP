bb76ce1e9890fba042cae2ee4508864ae66c5c89
MB-teacher <MB-teacher@MB-207-teacher>
2022-05-31 20:09:51 +0800

    ocp

--- src/main/java/day22_cocurrent/ExceptionTask.java
+++ src/main/java/day22_cocurrent/ExceptionTask.java
@@ -6,7 +6,7 @@ public class ExceptionTask implements Callable<String> {
 
     @Override
     public String call() throws Exception {
-        System.out.println("發生錯誤的 Task");
+        System.err.println("發生錯誤的 Task");
         throw new Exception("哇哇哇~發生了例外...");
     }
     
