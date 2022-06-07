5c8564c99193951b73e569742b86e6d3f6d0392b
MB-teacher <MB-teacher@MB-207-teacher>
2022-06-07 19:43:11 +0800

    ocp

---
+++ src/main/java/day24_invoke/AtomicIntegerDemo2.java
@@ -0,0 +1,31 @@
+package day24_invoke;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+class MyThread implements Runnable {
+    private static AtomicInteger count = new AtomicInteger();
+    @Override
+    public void run() {
+        int x = count.incrementAndGet();
+        System.out.print(x + " ");
+    }
+}
+
+public class AtomicIntegerDemo2 {
+    public static void main(String[] args) {
+        Thread t1 = new Thread(new MyThread());
+        Thread t2 = new Thread(new MyThread());
+        Thread t3 = new Thread(new MyThread());
+        Thread[] ta = {t1, t2, t3};
+        for(int x=0;x < 3; x++) {
+            ta[x].start();
+        }
+    }
+}
+/*
+Which statement is true?
+A. The program prints 1 2 3 and the order is unpredictable.
+B. The program prints 1 2 3.
+C. The program prints 1 1 1.
+D. A compilation error occurs.
+*/
