ced7c9e563bbdd8caf1b5c84841b08bb855b44e9
MB-teacher <MB-teacher@MB-207-teacher>
2022-06-07 21:28:36 +0800

    ocp

---
+++ src/main/java/day24_io/FileWriterDemo1.java
@@ -0,0 +1,28 @@
+package day24_io;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileWriterDemo1 {
+    public static void main(String[] args) {
+        FileWriter fw = null;
+        try {
+            fw = new FileWriter("src/main/java/day24_io/data/news.txt");
+            fw.append("Hello Java 爪哇");
+            fw.append("\n");
+            fw.append("Hello Web 網頁");
+            System.out.println("寫入完畢 !");
+        } catch (IOException e) {
+            System.out.println(e);
+        } finally {
+            if(fw != null) {
+                try {
+                    fw.close();
+                } catch (IOException e) {
+                    System.out.println(e);
+                }
+            }
+        }
+        
+    }
+}
