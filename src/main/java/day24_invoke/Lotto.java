f28a943727dfe1ef25a78c7e0cffbf6e3623d583
MB-teacher <MB-teacher@MB-207-teacher>
2022-06-07 18:53:52 +0800

    ocp

---
+++ src/main/java/day24_invoke/Lotto.java
@@ -0,0 +1,18 @@
+package day24_invoke;
+
+import java.util.Random;
+import java.util.concurrent.Callable;
+
+public class Lotto implements Callable<Integer>{
+
+    @Override
+    public Integer call() throws Exception {
+        Random random = new Random();
+        String tName = Thread.currentThread().getName();
+        Thread.sleep(random.nextInt(5000));
+        int num = random.nextInt(100);  // 0~99
+        System.out.printf("%s 搖出 %d\n", tName, num);
+        return num;
+    }
+    
+}
