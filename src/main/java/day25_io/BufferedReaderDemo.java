4bb9bc488807c4e43bc06ce7806a4a2399ad59c8
MB-teacher <MB-teacher@MB-207-teacher>
2022-06-09 19:29:29 +0800

    ocp

---
+++ src/main/java/day25_io/BufferedReaderDemo.java
@@ -0,0 +1,21 @@
+package day25_io;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class BufferedReaderDemo {
+    public static void main(String[] args) {
+        String path = "src/main/java/day24_io/data/news.txt";
+        try(FileReader fr = new FileReader(path);
+            BufferedReader br = new BufferedReader(fr);) {
+            
+            String data = null;
+            // 逐行讀取
+            while ((data = br.readLine()) != null) {
+                System.out.println(data);
+            }
+            
+        } catch (Exception e) {
+        }
+    }
+}
